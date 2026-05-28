c81629e8869adb5f9e2ad40f7d16cb43ea64ff53
lkett-personal <lkett-personal@protonmail.com>
2026-05-27 17:26:32 -0700

    added breaking tiles and refactored a bit i think?

--- scripts/map_generator.gd
+++ scripts/map_generator.gd
@@ -1,11 +1,5 @@
 extends TileMapLayer
 
-var map_width: int = 16
-var map_height: int = 16
-
-var dirt_weight: float = 0.33
-var stone_weight: float = 0.66
-
 const TERRAINS = {
 	"DIRT": 0,
 	"CLAY": 1,
@@ -13,61 +7,75 @@ const TERRAINS = {
 }
 
 const TILE_HEALTH = {
-	"DIRT": 3,
-	"CLAY": 5,
-	"STONE": 8
+	"DIRT": 2,
+	"CLAY": 3,
+	"STONE": 4
 }
 
-var terrain: int;
 var tile_health: Dictionary[Vector2i, int] = {}
+var broken_tiles: Dictionary[Vector2i, Array] = {}
+var tile_terrain: Dictionary[Vector2i, int] = {}
+
+var map_width: int = 16
+var map_height: int = 16
+var dirt_weight: float = 0.33
+var stone_weight: float = 0.66
 
 var power: int = 1
+var mine_cooldown: float = 0.1
 
 func _ready() -> void:
-	generate_noise(map_width, map_height)
+	generate_map(map_width, map_height)
 	
-func _physics_process(_delta: float) -> void:
-	mine_tile(power)
+func _physics_process(delta: float) -> void:
+	try_mine_tile(delta)
 
-func generate_noise(width: int, height: int) -> void:
+func generate_map(width: int, height: int) -> void:
 	var variation_noise := FastNoiseLite.new()
 	
 	variation_noise.seed = randi()
 	variation_noise.frequency = 0.05
 	
+	var cell_x = (width / 2)
+	var cell_y = (height / 2)
+
 	for x in width:
 		for y in height:
-			var variation_noise_raw := variation_noise.get_noise_2d(x, y)
-			var variation_noise_normalized := (variation_noise_raw + 1.0) / 2.0
-			
-			var cell_x = (width / 2)
-			var cell_y = (height / 2)
-			
-			var cell = Vector2i(x - cell_x, y - cell_y)
-		
-			if variation_noise_normalized <= dirt_weight:
-				terrain = TERRAINS["DIRT"]
-				init_tile_health(Vector2i(cell), terrain)
-				set_cells_terrain_connect([cell], 0, TERRAINS["DIRT"])
-			elif variation_noise_normalized <= stone_weight:
-				terrain = TERRAINS["STONE"]
-				init_tile_health(Vector2i(cell), terrain)
-				set_cells_terrain_connect([cell], 0, TERRAINS["STONE"])
+			var noise_val := (variation_noise.get_noise_2d(x, y) + 1.0) / 2.0
+			var cell := Vector2i(x - cell_x, y - cell_y)
+			var cell_terrain: int  # local, scoped to this iteration
+
+			if noise_val <= dirt_weight:
+				cell_terrain = TERRAINS["DIRT"]
+			elif noise_val <= stone_weight:
+				cell_terrain = TERRAINS["STONE"]
 			else:
-				terrain = TERRAINS["CLAY"]
-				init_tile_health(Vector2i(cell), terrain)
-				set_cells_terrain_connect([cell], 0, TERRAINS["CLAY"])
+				cell_terrain = TERRAINS["CLAY"]
+
+			init_tile_health(cell, cell_terrain)
+			generate_tile(cell, cell_terrain, false)
+
+			if cell_terrain == TERRAINS["STONE"] and randf() <= 0.1:
+				generate_tile(cell, cell_terrain, true)
 				
-			await animate_tile(Vector2i(cell), terrain)	
+			await animate_tile(Vector2i(cell))
 			
-func animate_tile(cell: Vector2i, _terrain_set: int) -> void:
-	var tween = create_tween()
-	var tmp_sprite = Sprite2D.new()
+func generate_tile(cell: Vector2i, terrain_id: int, is_ore: bool) -> void:
+	var ore_decision: int = randi_range(1, 3)
+	
+	if not is_ore:
+		set_cells_terrain_connect([cell], 0, terrain_id)
+	else:
+		set_cell(cell, 0, Vector2i(ore_decision, 12))
+			
+func animate_tile(cell: Vector2i) -> void:
 	
 	var source_id := get_cell_source_id(cell)
 	
 	if source_id == -1:
 		return
+	
+	var tween = create_tween()
 		
 	var source : TileSetAtlasSource = tile_set.get_source(source_id)
 	
@@ -78,6 +86,7 @@ func animate_tile(cell: Vector2i, _terrain_set: int) -> void:
 	var tile_image: Image = full_image.get_region(tile_region)
 	var image_tex := ImageTexture.create_from_image(tile_image)
 	
+	var tmp_sprite = Sprite2D.new()
 	tmp_sprite.texture = image_tex
 	tmp_sprite.position = map_to_local(cell)
 	tmp_sprite.scale = Vector2.ZERO 
@@ -85,10 +94,17 @@ func animate_tile(cell: Vector2i, _terrain_set: int) -> void:
 	add_child(tmp_sprite)
 	
 	tween.tween_property(tmp_sprite, "scale", Vector2.ONE * 1.4, 0.04)
-	tween.tween_property(tmp_sprite, "scale", Vector2.ONE, 0.1)
+	tween.tween_property(tmp_sprite, "scale", Vector2.ONE, 0.04)
 	await tween.finished
 	
 	tmp_sprite.queue_free()
+	
+func try_mine_tile(delta: float) -> void:
+	if mine_cooldown > 0:
+		mine_cooldown -= delta
+	else:
+		mine_cooldown = 0.1
+		mine_tile(power)
 	
 func mine_tile(damage: int) -> void:
 	var mouse_pos_global: Vector2 = get_global_mouse_position()
@@ -97,31 +113,27 @@ func mine_tile(damage: int) -> void:
 	
 	if mouse_pos_tile in tile_health:
 		damage_tile(mouse_pos_tile, damage)
-		print("kill dat boy!!")
 
 func damage_tile(cell: Vector2i, damage: int) -> void:
-	var health = get_tile_health(cell)
-	
 	tile_health[cell] -= damage
-	
-	if health <= 0:
+	if tile_health[cell] <= 0:
 		break_tile(cell)
+	else:
+		animate_tile(cell)
 	
-func break_tile(cell: Vector2i):
+func break_tile(cell: Vector2i) -> void:
+	var terrain_id = tile_terrain.get(cell, -1)
 	tile_health.erase(cell)
+	tile_terrain.erase(cell)
 	set_cells_terrain_connect([cell], 0, -1, false)
+	if terrain_id != -1:
+		broken_tiles[cell] = [3.0, terrain_id]
 	
 func init_tile_health(cell: Vector2i, terrain_id: int) -> void:
-	if terrain_id == TERRAINS["DIRT"]:
-		tile_health[cell] = 3
-	elif terrain_id == TERRAINS["CLAY"]:
-		tile_health[cell] = 4
-	elif terrain_id == TERRAINS["STONE"]:
-		tile_health[cell] = 5
-	
-func set_tile_health(cell: Vector2i, health: int) -> void:
-	if cell in tile_health:
-		tile_health[cell] = health
+	var terrain_name = TERRAINS.find_key(terrain_id)
+	if terrain_name and terrain_name in TILE_HEALTH:
+		tile_health[cell] = TILE_HEALTH[terrain_name]
+		tile_terrain[cell] = terrain_id
 		
 func get_tile_health(cell: Vector2i) -> int:
 	if cell in tile_health:
